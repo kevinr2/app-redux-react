8a07d3b21727948d7847e84f2d0778478409cf2b
kevinr2 <kevinreve096@gmail.com>
2021-10-11 11:06:41 -0500

    buscador

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+//bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+//img
+import imagen from './img/pokemon.jpg';
+import Store from './redux/Store';
+import {Provider} from 'react-redux'
+import Compras from './components/Compras.hook';
+import Cantidad from './components/Cantidad.hook';
+import BuscadorPokemon from './components/buscador/BuscadorPokemon';
+import ResultadoPokemon from './components/buscador/ResultadoPokemon';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={Store}>
+      <div className="App container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card mt-5" style={{maxWidth:'370px'}}>
+              <div className="row no-gutters">
+                <div className="col-4">
+                  <img src={imagen} alt="pokemon" className="card-img"/>
+                </div>
+                <div className="col-8">
+                  <div className="card-body">
+                    <div className="card-tile h3 text-center">
+                      <Cantidad/>
+                    </div>
+                      <Compras/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 mt-4 border-top pt-3">
+            <BuscadorPokemon/>
+          </div>
+          <div className='col-12'>
+            <ResultadoPokemon/>
+          </div>
+        </div>
+      </div>
+    </Provider>
   );
 }
 
